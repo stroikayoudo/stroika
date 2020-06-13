f6ad011866f765e25e8717d009ee3b4c93502ea6
oqyshi <artemovanvar2@gmail.com>
2020-06-13 13:45:15 +0600

    permission+categories

--- adverts/urls.py
+++ adverts/urls.py
@@ -6,6 +6,6 @@ app_name = 'adverts'
 urlpatterns = [
     path('', views.MainListView.as_view(), name='All'),
     path('<int:pk>', views.Category1DetailView.as_view(), name='detail'),
-    path('category2/<int:pk>', views.Category2DetailView.as_view(), name='detail'),
+    path('category2/<int:pk>', views.Category2DetailView.as_view(), name='detail2'),
 
 ]
